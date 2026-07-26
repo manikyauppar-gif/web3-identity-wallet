9b28baf238a4ca4bb68fbad7f72d2780e583119e
Manikya Uppar <manikya@gmail.com>
2026-07-26 15:23:10 +0530

    Update server.js for live deployment

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // Wrapper to run the identity-wallet server from the root directory
 const path = require('path');
 
-// Change the current working directory to the identity-wallet directory so that
-// dotenv, express.static, and relative file paths resolve correctly.
+// Change working directory to 'identity-wallet' so dotenv and static paths resolve correctly
 process.chdir(path.join(__dirname, 'identity-wallet'));
 
-// Require the actual server. Node will resolve all dependencies (express, cors, mongoose, etc.)
-// from the identity-wallet/node_modules folder relative to the server.js location.
-require('./identity-wallet/server.js');
+// Require server.js relative to the new working directory
+require('./server.js');
